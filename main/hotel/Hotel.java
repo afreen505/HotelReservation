d3bb7edaf1cc10a23cc5dac49df8d90c69103e85
afreen505 <afreen.fathimaa9876@gmail.com>
2021-09-02 14:29:37 +0530

    Uc2 Executed Successfully

--- main/hotel/Hotel.java
+++ main/hotel/Hotel.java
@@ -5,10 +5,21 @@ public class Hotel {
     private String hotelName;
     private int regularRate;
 
-    //Parameterized Constructor
-    public Hotel(String hotelName, int regularRate) {
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
         this.hotelName = hotelName;
+    }
+
+    public int getRegularRate() {
+        return regularRate;
+    }
+
+    public void setRegularRate(int regularRate) {
         this.regularRate = regularRate;
     }
+
 
 }
--- main/hotel/HotelReservation.java
+++ main/hotel/HotelReservation.java
@@ -3,13 +3,13 @@ package hotel;
 import java.util.ArrayList;
 
 public class HotelReservation {
-    ArrayList<Hotel> hotelList = new ArrayList<>();
+    ArrayList<Hotel> hotelList = new ArrayList<>(); //Created Arraylist as hotelList
 
     public static void main(String[] args) {
         System.out.println("Welcome to Hotel");
     }
 
-    public void addHotelDetails() {
+    public void addHotelDetails() {                //Created method addHotelDetails
         hotelList.add(new Hotel("Lakewood", 110));
         hotelList.add(new Hotel("Bridgewood", 150));
         hotelList.add(new Hotel("Ridgewood", 250));

d77b53909efc34a33498618c038915dbca0d13c3
afreen505 <58416095+afreen505@users.noreply.github.com>
2021-09-02 22:34:46 +0530

    Modified: Uc1 Added and Executed

--- Test/hotel/HotelReservationTest.java
+++ Test/hotel/HotelReservationTest.java
@@ -13,7 +13,7 @@ public class HotelReservationTest {
     }
 
     @Test
-<<<<<<< HEAD
+
     public void givenHotelDetails_whenAddedForHotel_Should_returnEnteredName() {
 
         Hotel newHotel = hotelReservation.addHotel("Lakewood", 110, 90);
@@ -32,11 +32,11 @@ public class HotelReservationTest {
 
         Hotel newHotel = hotelReservation.addHotel("Lakewood", 110, 90);
         assertEquals(newHotel.getWeekendRate(), 90);
-=======
+
     public void shouldPrintWelcomeMessage() {
         //Creating Object
         HotelReservation hotelReservation = new HotelReservation();
         hotelReservation.addHotelDetails();
->>>>>>> Uc1_AddHotel
+ 
     }
 }
